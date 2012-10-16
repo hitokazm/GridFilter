c031dee0bd693c3fccd7d62ad808a6f5524dc52b
hitokazm <hitokazu@gmail.com>
2012-10-16 11:53:01 -0600

    grid_filter_gl test

--- GridFilter/src/gridfilter.py
+++ GridFilter/src/gridfilter.py
@@ -3,3 +3,18 @@ Created on Oct 16, 2012
 
 @author: hitokazu
 '''
+
+import numpy as np
+from grid_filter_gl import *
+
+init_window(100,100)
+
+grid = np.array(np.mat('1 0; 0 1'), ndmin=2)
+
+update_grid(grid)
+
+n = 0
+
+while n < 10000000000000: 
+    draw_grid()
+    n += 1
